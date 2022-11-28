31713d4853e25b1b496681df46805378139cfc50
Aku Kauste <aku.kauste@mediaviisi.fi>
2022-11-28 15:49:21 +0200

    Missed the new files...

---
+++ commands/create.test.js
@@ -0,0 +1,24 @@
+import {describe, expect, test} from '@jest/globals';
+import {splitPath} from './create.js';
+
+describe('create', () => {
+  describe('splitPath(path)', () => {
+    test('./some/simple/Path', () => {
+      const [path, baseName] = splitPath('./some/simple/Path');
+      expect(path).toBe('./some/simple');
+      expect(baseName).toBe('Path');
+    });
+
+    test('ignore extension ./some/simple/Path.js', () => {
+      const [path, baseName] = splitPath('./some/simple/Path.js');
+      expect(path).toBe('./some/simple');
+      expect(baseName).toBe('Path');
+    });
+    
+    test('ignore multipart extension ./some/other/Component.test.js', () => {
+      const [path, baseName] = splitPath('./some/other/Component.test.js');
+      expect(path).toBe('./some/other');
+      expect(baseName).toBe('Component');
+    });
+  });
+});
